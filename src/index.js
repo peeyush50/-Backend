e6bbd4821c9d7a5bd10ad5d7a15efa466b0b9c0a
peeyush gupta <peeyushgupta279@gmail.com>
2025-03-06 23:35:52 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -38,3 +38,11 @@ import connectDB from './db/index.js'
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port:${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!! ",err);
+})
