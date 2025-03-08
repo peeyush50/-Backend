22cb6e0970b47f49e3df6f620b2060f374b5b95f
peeyush gupta <peeyushgupta279@gmail.com>
2025-03-08 14:13:14 +0530

    postman

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -21,6 +21,7 @@ const uploadOnCloudinary =async (localFilePath)=>{
       })
       //file has been upload successfully
       console.log("file is uploaded on cloudinary" , response.url);
+      fs.unlinkSync(localFilePath);
       return response;
 
     }
