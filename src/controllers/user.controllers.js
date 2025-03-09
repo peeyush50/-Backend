20b75ef03ba2c71a486fac5cb4e818d517cc242a
peeyush gupta <peeyushgupta279@gmail.com>
2025-03-09 14:06:32 +0530

    subscription model

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -208,7 +208,7 @@ const logoutUser = asyncHandler(async (req,res)=>{
 const refreshAccessToken = asyncHandler (async (req,res)=>{
   const incomingRefreshToken= req.cookies.refreshToken || req.body.refreshtoken
 
- if(incomingRefreshToken){
+ if(!incomingRefreshToken){
     throw new ApiError(401, "unauthorized request")
  }
 
